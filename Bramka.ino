1e432213b953fdfaae2865369803a5d0fa44bd2d
Leszek <leszek.chorazewicz@gmail.com>
2018-04-16 00:40:39 +0200

    parametry

--- Bramka.ino
+++ Bramka.ino
@@ -1,6 +1,7 @@
 #include <SoftwareSerial.h>
 
 const int PIN_TXD = 4, PIN_RXD = 5, PIN_AKTYWUJACY = 3, CZESTOTLIWOSC = 9600;
+String telefon, wiadomosc;
 
 SoftwareSerial Gsm(PIN_TXD, PIN_RXD);
 
@@ -11,25 +12,53 @@ void setup() {
 }
 
 void loop() {
-  if(digitalRead(PIN_AKTYWUJACY) == HIGH){
-    Serial.write("\nWyslalem Smsa\n");
+//  if(digitalRead(PIN_AKTYWUJACY) == HIGH){
     wyslijSms();
-  }
+//  }
   while(Gsm.available()){
     Serial.write(Gsm.read());
   }
   while(Serial.available()){
     Gsm.write(Serial.read());
   }
+  delay(500);
 }
 
 void wyslijSms(){
-  Gsm.write("AT+CMGF=1\r\n");
+  Serial.write("\nRozpoczynam tworzenie wiadomości sms\n\n");
+  
+  String telefon = 
+      WyswietlWiadomoscNaMonitorzePortuSzeregowegoIOczekujNaOdpowiedz("Wprowadz numer telefonu> ");
+  Serial.println(">"+telefon+"\n");
+  String wiadomosc = 
+      WyswietlWiadomoscNaMonitorzePortuSzeregowegoIOczekujNaOdpowiedz("Wprowadz treść wiadomości> ");
+  Serial.println(">"+wiadomosc+"\n");
+  Gsm.println("AT+CMGF=1\r\n");
   delay(100);
-  Gsm.write("AT+CMGS=\"+48xxxxxxxxx\"\r\n");
+
+  Gsm.write("AT+CMGS=\"+48513210196\"\r\n");
+
   delay(100);
-  Gsm.write("Test bramki sms");
+  Gsm.println(wiadomosc);
   delay(100);
   Gsm.write((char)26);
-  delay(100);   
+  delay(100);
 }
+
+String WyswietlWiadomoscNaMonitorzePortuSzeregowegoIOczekujNaOdpowiedz(String wiadomosc){
+  Serial.println(wiadomosc);
+  czekajNaOdpowiedz();
+  return Serial.readString();
+}
+
+void czekajNaOdpowiedz(){
+  while(Serial.available() == 0){} 
+}
+
+void writeString(String stringData) {
+  for (int i = 0; i < stringData.length(); i++)
+  {
+    Gsm.write(stringData[i]);
+  }
+}
+
